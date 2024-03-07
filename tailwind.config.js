9dbf49f4bc6d71ed188114984ace8d8e268bd092
Hélvio Filho <helviosvf@gmail.com>
2024-03-07 15:14:10 -0300

    chore: configure custom fonts and colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import { colors } from "./src/theme/colors";
+import { fontFamily } from "./src/theme/fontFamily";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontFamily,
+    },
   },
   plugins: [],
 };
