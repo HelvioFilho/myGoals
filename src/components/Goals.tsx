098280843e8e2ac8f6a620a6034edc2eef132304
Hélvio Filho <helviosvf@gmail.com>
2024-03-07 18:10:52 -0300

    feat: create Goals component

---
+++ src/components/Goals.tsx
@@ -0,0 +1,47 @@
+import { ScrollView, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { Goal } from "./Goal";
+
+import { colors } from "@/theme/colors";
+
+export type GoalsProps = {
+  id: string;
+  name: string;
+  current: number;
+  total: number;
+}[];
+
+type Props = {
+  goals: GoalsProps;
+  onPress: (id: string) => void;
+  onAdd: () => void;
+};
+
+export function Goals({ goals, onPress, onAdd }: Props) {
+  return (
+    <ScrollView
+      className="w-full max-h-44"
+      horizontal
+      contentContainerClassName="gap-4"
+      showsHorizontalScrollIndicator={false}
+    >
+      <TouchableOpacity
+        className="bg-green-500 w-16 max-h-44 items-center justify-center rounded-lg"
+        activeOpacity={0.8}
+        onPress={onAdd}
+        testID="add-button"
+      >
+        <MaterialIcons name="add" size={36} color={colors.black} />
+      </TouchableOpacity>
+
+      {goals.map(({ id, name, current, total }) => (
+        <Goal
+          key={id}
+          goal={{ name, current, total }}
+          onPress={() => onPress(id)}
+        />
+      ))}
+    </ScrollView>
+  );
+}
