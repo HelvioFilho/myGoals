37fc2aab7c9b323582e95debc2e9b493cc7052e9
Hélvio Filho <helviosvf@gmail.com>
2024-03-10 19:57:31 -0300

    feat: creates a hook to insert goals into the database

---
+++ src/storage/useGoalRepository.ts
@@ -0,0 +1,29 @@
+import { useSQLiteContext } from "expo-sqlite/next";
+
+export type GoalCreateDatabase = {
+  name: string;
+  total: number;
+};
+
+export function useGoalRepository() {
+  const database = useSQLiteContext();
+
+  function create(goal: GoalCreateDatabase) {
+    try {
+      const statement = database.prepareSync(
+        "INSERT INTO goals (name, total) VALUES ($name, $total)"
+      );
+
+      statement.executeSync({
+        $name: goal.name,
+        $total: goal.total,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return {
+    create,
+  };
+}
