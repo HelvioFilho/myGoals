5adadbcd1e7f8388f28d284bc7b630127474abbd
Hélvio Filho <helviosvf@gmail.com>
2024-03-10 19:39:52 -0300

    feat: create the home screen showing transactions

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,150 @@
-import "@/theme/global.css";
+import { useEffect, useRef, useState } from "react";
+import { Alert, Keyboard, View } from "react-native";
+import Bottom from "@gorhom/bottom-sheet";
+import { useRouter } from "expo-router";
+import dayjs from "dayjs";
 
-import { Text, View } from "react-native";
+import { Input } from "@/components/Input";
+import { Header } from "@/components/Header";
+import { Button } from "@/components/Button";
+import { BottomSheet } from "@/components/BottomSheet";
+import { Goals, GoalsProps } from "@/components/Goals";
+import { Transactions, TransactionsProps } from "@/components/Transactions";
+
+import { mocks } from "@/utils/mocks";
 
 export default function Home() {
+  // const goals = [
+  //   {
+  //     id: "1",
+  //     name: "Comprar um Carro",
+  //     current: 10003.7,
+  //     total: 60000,
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Viagem",
+  //     current: 1500,
+  //     total: 2000,
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Trocar a memória do computador",
+  //     current: 20,
+  //     total: 375,
+  //   },
+  // ];
+
+  // const transactions = [
+  //   {
+  //     date: "01/01/2022",
+  //     amount: 100,
+  //   },
+  // ];
+
+  const [name, setName] = useState("");
+  const [total, setTotal] = useState("");
+  const [transactions, setTransactions] = useState<TransactionsProps>([]);
+  const [goals, setGoals] = useState<GoalsProps>([]);
+
+  const { navigate } = useRouter();
+
+  const bottomSheetRef = useRef<Bottom>(null);
+  const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
+  const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
+
+  async function handleCreate() {
+    try {
+      const totalAsNumber = Number(total.toString().replace(",", "."));
+
+      if (isNaN(totalAsNumber)) {
+        return Alert.alert("Erro", "Valor inválido.");
+      }
+
+      console.log({ name, total: totalAsNumber });
+
+      Keyboard.dismiss();
+      handleBottomSheetClose();
+      Alert.alert("Sucesso", "Meta cadastrada!");
+
+      setName("");
+      setTotal("");
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível cadastrar.");
+      console.log(error);
+    }
+  }
+
+  function handleDetails(id: string) {
+    navigate("/details/" + id);
+  }
+
+  async function fetchGoals() {
+    try {
+      const response = mocks.goals;
+      setGoals(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchTransactions() {
+    try {
+      const response = mocks.transactions;
+
+      setTransactions(
+        response.map((item) => ({
+          ...item,
+          date: dayjs(item.created_at).format("DD/MM/YYYY [às] HH:mm"),
+        }))
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchGoals();
+    fetchTransactions();
+  }, []);
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Home</Text>
+    <View className="flex-1 p-8">
+      <Header
+        title="Suas metas"
+        subtitle="Poupe hoje para colher os frutos amanhã."
+      />
+      <Goals
+        goals={goals}
+        onAdd={handleBottomSheetOpen}
+        onPress={handleDetails}
+      />
+
+      <Transactions transactions={transactions} />
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        title="Nova meta"
+        snapPoints={[0.01, 284]}
+        onClose={handleBottomSheetClose}
+      >
+        <Input
+          testID="name-input"
+          placeholder="Nome da meta"
+          onChangeText={setName}
+          value={name}
+        />
+
+        <Input
+          testID="total-input"
+          placeholder="Valor"
+          keyboardType="numeric"
+          onChangeText={setTotal}
+          value={total}
+        />
+
+        <Button testID="create-button" title="Criar" onPress={handleCreate} />
+      </BottomSheet>
     </View>
   );
 }
