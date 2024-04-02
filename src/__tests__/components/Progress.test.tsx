59a7156d5071d8796366aa81a4545434d12f2264
Hélvio Filho <helviosvf@gmail.com>
2024-04-02 12:18:33 -0300

    test: handle when progress is greater than 100 and when it is 0%

--- src/__tests__/components/Progress.test.tsx
+++ src/__tests__/components/Progress.test.tsx
@@ -3,12 +3,17 @@ import { render, screen } from "@testing-library/react-native";
 
 describe("Component: Progress", () => {
   it("should render correctly", () => {
-    render(<Progress percentage={60} />);
-    expect(screen.getByText("60%")).toBeTruthy();
+    render(<Progress percentage={110} />);
+    expect(screen.getByText("110%")).toBeTruthy();
   });
 
   it("should render correctly below 60%", () => {
     render(<Progress percentage={55} />);
     expect(screen.getByText("55%")).toBeTruthy();
+  });
+
+  it("should render when percentage is 0", () => {
+    render(<Progress percentage={0} />);
+    expect(screen.getByText("0%")).toBeTruthy();
   });
 });
