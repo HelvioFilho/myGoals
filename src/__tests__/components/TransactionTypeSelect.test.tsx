6abd6ce1c68b9383f395a604dc7717a37fadca3b
Hélvio Filho <helviosvf@gmail.com>
2024-04-02 12:20:10 -0300

    test: handles the state change between deposit and withdrawal

--- src/__tests__/components/TransactionTypeSelect.test.tsx
+++ src/__tests__/components/TransactionTypeSelect.test.tsx
@@ -1,6 +1,7 @@
 import { TransactionTypeSelect } from "@/components/TransactionTypeSelect";
 import { fireEvent, render, screen } from "@testing-library/react-native";
 
+const onChange = jest.fn();
 describe("Component: TransactionTypeSelect", () => {
   it("should render correctly", () => {
     render(<TransactionTypeSelect selected="up" onChange={jest.fn()} />);
@@ -9,15 +10,11 @@ describe("Component: TransactionTypeSelect", () => {
   });
 
   it("should call onChange correctly", () => {
-    const onChange = jest.fn();
     render(<TransactionTypeSelect selected="up" onChange={onChange} />);
     fireEvent.press(screen.getByText("Saque"));
     expect(onChange).toHaveBeenCalledTimes(1);
-  });
 
-  it("should render correctly selected false", () => {
-    render(<TransactionTypeSelect selected="down" onChange={jest.fn()} />);
-    expect(screen.getByText("Depósito")).toBeTruthy();
-    expect(screen.getByText("Saque")).toBeTruthy();
+    fireEvent.press(screen.getByText("Depósito"));
+    expect(onChange).toHaveBeenCalledTimes(2);
   });
 });
