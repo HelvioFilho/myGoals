2b58e842f9c150c0ca1b4374812b07eb3ff417bc
Hélvio Filho <helviosvf@gmail.com>
2024-04-11 19:17:30 -0300

    feat: update the goal

--- src/app/details/[id].tsx
+++ src/app/details/[id].tsx
@@ -1,8 +1,9 @@
 import dayjs from "dayjs";
 import { useEffect, useRef, useState } from "react";
-import { Alert, Keyboard, View } from "react-native";
+import { Alert, Keyboard, Text, TouchableOpacity, View } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import Bottom from "@gorhom/bottom-sheet";
+import { Ionicons } from "@expo/vector-icons";
 
 import { useGoalRepository } from "@/storage/useGoalRepository";
 import { useTransactionRepository } from "@/storage/useTransactionRepository";
@@ -27,11 +28,14 @@ type DetailsProps = {
   percentage: number;
   transactions: TransactionProps[];
 };
+
 export default function Details() {
   const [amount, setAmount] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [type, setType] = useState<"up" | "down">("up");
   const [goal, setGoal] = useState<DetailsProps>({} as DetailsProps);
+  const [name, setName] = useState("");
+  const [total, setTotal] = useState("");
 
   const useGoal = useGoalRepository();
   const useTransaction = useTransactionRepository();
@@ -41,8 +45,23 @@ export default function Details() {
   const { back } = useRouter();
 
   const bottomSheetRef = useRef<Bottom>(null);
+  const settingsBottomSheetRef = useRef<Bottom>(null);
+  const inputBottomSheetRef = useRef<Bottom>(null);
+
   const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
   const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
+
+  const handleSettingsBottomSheetOpen = () =>
+    settingsBottomSheetRef.current?.expand();
+  const handleSettingsBottomSheetClose = () =>
+    settingsBottomSheetRef.current?.snapToIndex(0);
+
+  const handleInputBottomSheetOpen = () => {
+    inputBottomSheetRef.current?.expand();
+  };
+  const handleInputBottomSheetClose = () => {
+    inputBottomSheetRef.current?.snapToIndex(0);
+  };
 
   function fetchDetails() {
     try {
@@ -72,7 +91,8 @@ export default function Details() {
           date: dayjs(item.created_at).format("DD/MM/YYYY [às] HH:mm"),
         })),
       });
-
+      setName(goal.name);
+      setTotal(goal.total.toFixed(2).replace(".", ",").toString());
       setIsLoading(false);
     } catch (error) {
       Alert.alert("Erro", "Não foi possível carregar as transações.");
@@ -107,6 +127,43 @@ export default function Details() {
     }
   }
 
+  function handleGoalChange() {
+    try {
+      const totalAsNumber = Number(total.toString().replace(",", "."));
+
+      if (isNaN(totalAsNumber)) {
+        const newTotal = parseFloat(
+          goal.total.replace(/[^\d,]/g, "").replace(",", ".")
+        )
+          .toFixed(2)
+          .replace(".", ",")
+          .toString();
+        setTotal(newTotal);
+        return Alert.alert("Erro", "Valor da meta deve ser um número válido.");
+      }
+
+      if (name === "" || totalAsNumber === 0) {
+        setName(goal.name);
+        return Alert.alert(
+          "Erro",
+          "Nome e valor da meta devem ser informados."
+        );
+      }
+
+      useGoal.update({
+        id: goalId,
+        name,
+        total: Number(total.replace(",", ".")),
+      });
+
+      fetchDetails();
+      handleInputBottomSheetClose();
+      Alert.alert("Sucesso", "Meta Alterada com sucesso!");
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível editar a meta.");
+    }
+  }
+
   useEffect(() => {
     fetchDetails();
   }, []);
@@ -117,8 +174,15 @@ export default function Details() {
 
   return (
     <View className="flex-1 p-8 pt-12">
-      <View className="mt-8">
+      <View className="mt-8 flex-row items-center justify-between">
         <BackButton />
+        <TouchableOpacity
+          className="p-2 rounded-full items-center justify-center"
+          activeOpacity={0.7}
+          onPress={handleSettingsBottomSheetOpen}
+        >
+          <Ionicons name="settings" size={32} color="white" />
+        </TouchableOpacity>
       </View>
 
       <Header title={goal.name} subtitle={`${goal.current} de ${goal.total}`} />
@@ -129,10 +193,61 @@ export default function Details() {
 
       <Button
         testID="add-transaction-button"
-        title="Nova transação"
+        title="Adicionar nova transação"
         onPress={handleBottomSheetOpen}
       />
-
+      <BottomSheet
+        ref={inputBottomSheetRef}
+        title="Editar Meta"
+        snapPoints={[0.01, 580]}
+        onClose={handleInputBottomSheetClose}
+      >
+        <Text className="font-semiBold text-lg text-gray-100">
+          Nome da Meta
+        </Text>
+        <Input
+          testID="name-input"
+          placeholder="Nome da meta"
+          onChangeText={setName}
+          value={name}
+        />
+        <Text className="font-semiBold text-lg text-gray-100 mt-3">
+          Valor total que deseja arrecadar
+        </Text>
+        <Input
+          testID="total-input"
+          placeholder="Valor"
+          keyboardType="numeric"
+          onChangeText={setTotal}
+          value={total}
+        />
+        <Button
+          testID="change-button"
+          title="Alterar"
+          onPress={handleGoalChange}
+        />
+      </BottomSheet>
+      <BottomSheet
+        ref={settingsBottomSheetRef}
+        title="Configurações"
+        snapPoints={[0.01, 284]}
+        onClose={handleSettingsBottomSheetClose}
+      >
+        <View className="flex-row gap-4 justify-around mt-8">
+          <Button
+            className="w-28 h-12 bg-blue-500 items-center justify-center rounded-sm"
+            title="Editar"
+            onPress={() => {
+              handleSettingsBottomSheetClose();
+              handleInputBottomSheetOpen();
+            }}
+          />
+          <Button
+            className="w-28 h-12 bg-red-500 items-center justify-center rounded-sm"
+            title="Excluir"
+          />
+        </View>
+      </BottomSheet>
       <BottomSheet
         ref={bottomSheetRef}
         title="Nova transação"
